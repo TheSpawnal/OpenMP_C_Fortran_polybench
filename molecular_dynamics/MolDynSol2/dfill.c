614ff84404dec4d588de2513f8f3a593da0f944d
ALJDBS <jdbs.pra@uahoo.com>
2025-11-09 16:49:15 +0100

    molDyn2--

---
+++ molecular_dynamics/MolDynSol2/dfill.c
@@ -0,0 +1,8 @@
+
+//  initialises double precision array to scalar value
+void dfill(int n, double val, double a[], int ia){
+    int i;
+
+    for(i=0; i < (n-1)*ia+1;i+=ia)
+        a[i] = val;
+}
