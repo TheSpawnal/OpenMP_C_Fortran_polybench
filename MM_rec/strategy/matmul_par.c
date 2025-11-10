877cc2f2c87749a5e9aec81b6e61875c1ee8d334
ALJDBS <jdbs.pra@uahoo.com>
2025-11-10 19:09:28 +0100

    mm_rc

---
+++ MM_rec/strategy/matmul_par.c
@@ -0,0 +1,11 @@
+// Parallel Matrix Multiply (using OpenMP)
+// **
+// **  PURPOSE: This is a simple matrix multiply program. 
+// **           It will compute the product
+// **
+// **                C  = A * B
+// **
+// **           A and B are set to constant matrices so we
+// **           can make a quick test of the multiplication.
+// **
+
